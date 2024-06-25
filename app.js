dfae4d995d45357a4ade33c4e47900011cdf825b
Glatia <88858289+Glatia@users.noreply.github.com>
2024-06-24 19:54:47 -0700

    Rewrite auth/create auth
    Rewrote the auth and create auth functions based off of async functions and added a logout/delete account button on the home page

--- app.js
+++ app.js
@@ -15,15 +15,19 @@ const bcrypt = require('bcrypt');
 
 
 app.use(express.urlencoded({
+
     extended: true
+
 }));
 
 app.use(session({
+
     secret: "secret",
     resave: true,
     username: '',
     logged_in: false,
     saveUninitialized: true
+
 }))
 
 app.use(express.static('public'));
@@ -32,167 +36,164 @@ app.set('view engine', 'ejs');
 
 var accounts;
 
-var title;
+async function update_data() {
 
-function update_data() {
-    fs.readFile('accounts.json', (error, data) => {
-        if (error) throw error;
-        else accounts = JSON.parse(data);
-        console.log(accounts)
+    return new Promise(async (resolve, reject) => {
+
+        accounts = await JSON.parse(await fs.readFileSync('accounts.json', 'utf-8'))
+        resolve(accounts)
+
     })
 }
 
 
+            password: password_hash,
+            id: Object.keys(accounts).length + 1
+
+        }
+
+        return accounts
+
+    } catch (err) {
+
+        throw err
+
+    }
+
+}
 
 io.on('connection', (socket) => {
+
     console.log('A user has connected')
+
 })
 
 
 app.get('/', (req, res) => {
+
     res.redirect("/login");
+
 })
 
 app.get('/login', (req, res) => {
+
     res.render("login")
+
 })
 
 app.get('/create', (req, res) => {
+
     res.render("create")
+
 })
 
 app.get('/home', (req, res) => {
+
     if (req.session.logged_in) {
         res.render("home")
+
     } else {
         res.redirect("/login")
+
     }
 })
 
+app.get("/delete", async (req, res) => {
+
+    await update_data()
+
+    delete accounts[req.session.username]
+
+    fs.writeFileSync('accounts.json', JSON.stringify(accounts))
+
+    req.session.logged_in = false
+    req.session.username = ""
+    req.session.save()
+
+})
 
 app.get("/logout", (req, res) => {
+
     req.session.logged_in = false
     req.session.username = ""
     res.session.save()
+
 })
 
 app.get("/home_auth", (req, res) => {
-    res.json({session: req.session})
+
+    res.json({
+        session: req.session
+    })
+
 })
 
-app.post('/create_auth', (req, res) => {
-    update_data()
-    const { username, password } = req.body
-    setTimeout(() => {
-        const create_auth = new Promise((resolve, reject) => {
-            let users = []
-            accounts.forEach(account => {
-                users.push(account.username);
+app.post('/create_auth', async (req, res) => {
+
+    const {
+        username,
+        password
+    } = req.body
+
+    try {
+
+        const new_accounts = await create_account(username, password)
+
+        await fs.writeFileSync("accounts.json", JSON.stringify(new_accounts, null, 2))
+
+        req.session.username = username
+        req.session.logged_in = true
+        req.session.save(() => {
+
+            res.json({
+
+                title: "Welcome!",
+                redirect: true
+
             })
-            if (users.includes(username)) {
-                reject('Username Taken');
-            } else {
-                resolve('Account Created');
-            }
         })
-        create_auth.then(
-            (value) => {
-                console.log(value)
-                bcrypt.hash(password, 10, (err, hash) => {
 
-                    accounts[accounts.length] = {
-                        username: username,
-                        password: hash
-                    }
-                    fs.writeFileSync('accounts.json', JSON.stringify(accounts, null, 2));
-                    req.session.username = username;
-                    req.session.logged_in = true;
-                    req.session.save(() => {
-                        console.log(req.session)
-                    })
-                    res.json({
-                        title: "Logged In!",
-                        redirect: true
-                    })
-                })
-            }, (error) => {
-                console.log(error);
-                req.session.username = username;
-                req.session.logged_in = false;
-                req.session.save(async () => {
-                    console.log(req.session)
-                })
-                res.json({
-                    title: error,
-                    redirect: false
-                })
-            }
-        )
-    }, 10)
+    } catch (err) {
+        res.json({
+            title: err.message,
+            redirect: false
+        })
+    }
+
 })
 
-app.post('/auth', (req, res) => {
-    update_data()
-    const { username, password } = req.body
+app.post('/auth', async (req, res) => {
 
-    setTimeout(() => {
-        const check_auth = new Promise((resolve, reject) => {
-            let i = 0
+    const {
+        username,
+        password
+    } = req.body;
 
-            if (accounts.length > 0) {
-                accounts.forEach((account) => {
-                    if (account.username == username) {
-                        bcrypt.compare(password, account.password, (err, same) => {
-                            if (err) throw err;
-                            else {
-                                if (same) {
-                                    resolve("Logged In")
-                                } else {
-                                    reject("Password Incorrect")
-                                }
-                            }
-                        })
-                    } else {
-                        i++
-                        if (i >= accounts.length) {
-                            reject("Username Invalid")
-                        }
-                    }
-                })
-            } else {
-                reject("Username Invalid")
-            }
+    try {
+
+        const authorized = await authenticate_account(username, password);
+
+        req.session.username = username;
+        req.session.logged_in = true;
+        req.session.save(() => {
+
+            res.json({
+
+                title: "Logged In!",
+                redirect: true
+
+            })
+        });
+
+    } catch (err) {
+
+        res.json({
+
+            title: err.message,
+            redirect: false
+
         })
-        check_auth.then(
-            (value) => {
-                console.log(value);
-                req.session.username = req.body.username
-                req.session.logged_in = true;
-                req.session.save(() => {
-                    console.log(req.session)
-                })
-
-                res.json({
-                    title: "Logged In!",
-                    redirect: true
-                })
-
-            }, 
-            (error) => {
-                console.log(error)
-                req.session.username = req.body.username;
-                req.session.logged_in = false
-                req.session.save(() => {
-                    console.log(req.session)
-                })
-
-                res.json({
-                    title: error,
-                    redirect: false
-                })
-            }
-        )
-    }, 10)
+    }
 })
 
 
